ed399678adfc25f9940bec8e39a497ffc9bf0adb
彭镜肇 <2705696192@qq.com>
2024-03-27 13:42:12 +0800

    :sparkles: feat(Http): 新增post请求

--- small-group/Week_2/http-demo-frontent/src/views/Http.vue
+++ small-group/Week_2/http-demo-frontent/src/views/Http.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <el-button @click="getVis=true">发送 GET 请求</el-button>
-    <el-button @click="sendPostRequest">发送 POST 请求</el-button>
-    <el-button @click="sendPutRequest">发送 PUT 请求</el-button>
-    <el-button @click="sendDeleteRequest">发送 DELETE 请求</el-button>
-    <el-button @click="sendPatchRequest">发送 PATCH 请求</el-button>
+    <el-button @click="postVis=true">发送 POST 请求</el-button>
+    <el-button @click="putVis=true">发送 PUT 请求</el-button>
+    <el-button @click="deleteVis=true">发送 DELETE 请求</el-button>
+    <el-button @click="patchVis=true">发送 PATCH 请求</el-button>
   </div>
 
   <el-dialog :model-value="getVis" title="get" :show-close="false" :close-on-click-modal="false">
@@ -18,22 +18,26 @@
 
     <span slot="footer" class="dialog-footer">
         <el-button type="danger" size="small" @click="getVis = false">关闭</el-button>
-        <el-button type="primary" size="small" @click="sendGetRequest">保存</el-button>
+        <el-button type="primary" size="small" @click="sendGetRequest">发送</el-button>
       </span>
   </el-dialog>
 
-  <el-dialog :model-value="getVis" title="get" :show-close="false" :close-on-click-modal="false">
+  <el-dialog :model-value="postVis" title="post" :show-close="false" :close-on-click-modal="false">
 
     <el-form class="inner-form" :model="user">
       <el-form-item label="用户名:">
         <el-input v-model="user.username"></el-input>
       </el-form-item>
 
+      <el-form-item label="密码:">
+        <el-input v-model="user.password"></el-input>
+      </el-form-item>
+
     </el-form>
 
     <span slot="footer" class="dialog-footer">
-        <el-button type="danger" size="small" @click="getVis = false">关闭</el-button>
-        <el-button type="primary" size="small" @click="sendGetRequest">保存</el-button>
+        <el-button type="danger" size="small" @click="postVis = false">关闭</el-button>
+        <el-button type="primary" size="small" @click="sendPostRequest">发送</el-button>
       </span>
   </el-dialog>
 
