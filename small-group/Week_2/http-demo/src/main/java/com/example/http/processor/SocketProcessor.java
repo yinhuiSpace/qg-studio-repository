4dedb0700a3c1e15346e7c78295b75adce1005b5
彭镜肇 <2705696192@qq.com>
2024-03-27 12:47:00 +0800

    :sparkles: feat(SocketProcessor): 新增解析请求体

--- small-group/Week_2/http-demo/src/main/java/com/example/http/processor/SocketProcessor.java
+++ small-group/Week_2/http-demo/src/main/java/com/example/http/processor/SocketProcessor.java
@@ -2,9 +2,12 @@ package com.example.http.processor;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.example.http.request.HttpRequest;
+import com.example.http.response.HttpResponse;
+import com.example.http.result.Result;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +19,7 @@ import java.util.Map;
  * @description: 专门用于解析客户端传来的数据流，找出并封装有用的信息
  * @date 2024/3/22 19:54
  */
-public class SocketProcessor implements Runnable{
+public class SocketProcessor implements Runnable {
 
     //
     private Socket socket;
@@ -33,7 +36,6 @@ public class SocketProcessor implements Runnable{
     private HttpRequest request;
 
 
-
     public SocketProcessor(Socket socket) {
         this.socket = socket;
     }
@@ -44,18 +46,24 @@ public class SocketProcessor implements Runnable{
     }
 
     /**
-    * 处理请求：包括确定请求方式，依次分类处理，最终要封装一个对象，并且要根据客户端需要返回数据
-    * */
-    public void processReq(){
+     * 处理请求：包括确定请求方式，依次分类处理，最终要封装一个对象，并且要根据客户端需要返回数据
+     */
+    public void processReq() {
 
         try {
-            request=new HttpRequest();
+            request = new HttpRequest();
 
             //初步依据http格式，解析并划分请求组成部分，分不同部分处理
             parseReq();
 
             //解析出请求行
             getReqLine();
+
+            //解析请求头
+            getHeader();
+
+            //封装响应对象并返回给客户端
+            sendResp();
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -64,9 +72,9 @@ public class SocketProcessor implements Runnable{
     }
 
     /**
-    * 解析请求
-    * */
-    public void parseReq()throws Exception{
+     * 解析请求
+     */
+    public void parseReq() throws Exception {
         //接收请求数据
         InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
         BufferedReader reader = new BufferedReader(inputStreamReader);
@@ -81,18 +89,52 @@ public class SocketProcessor implements Runnable{
         String[] reqInLines = reqStr.split("[\r\n]");
         //现在就从数据流中解析出一行一行的数据了
         //第一行肯定是请求行
-        reqLine=reqInLines[0];
+        reqLine = reqInLines[0];
 
         //大括号括起来的肯定是请求体
+        if (reqStr.contains("{")) {
+            String reqBodyStr = reqStr.split("\\{")[1];
 
+            reqBody = "{" + reqBodyStr;
+        }
 
         //夹在中间的就是请求头了
+
+        if (reqStr.contains("{")) {
+            reqHeader = reqStr.substring(reqStr.indexOf("\r\n") + 1, reqStr.indexOf("{"));
+
+            //解析请求体
+            getReqBody();
+        }
+    }
+
+    private void getHeader() {
+        //提取出请求头中的键值对
+        String[] paramEntry = reqHeader.split("[\r\n]");
+
+        HashMap<String, Object> headerMap = new HashMap<>();
+
+        for (String entry : paramEntry) {
+            if (!entry.contains(":")) {
+                continue;
+            }
+            //以冒号分割
+            String[] entryStr = entry.split(":");
+
+            String key = entryStr[0];
+            String val = entryStr[1];
+
+            headerMap.put(key, val);
+        }
+
+        //封装进请求对象中
+        request.setParams(headerMap);
     }
 
 
     /**
-    * 解析请求行
-    * */
+     * 解析请求行
+     */
     private void getReqLine() {
         //解析请求行，先以空格分割，按照http格式，一共存在三部分：请求方式，请求路径（get请求会在路径后拼接参数），请求协议
         String[] lineSplit = reqLine.split(" ");
@@ -101,7 +143,7 @@ public class SocketProcessor implements Runnable{
         //第二个是请求路径，还可能有get请求的参数
         //参数map
         HashMap<String, Object> paramMap = new HashMap<>();
-        if (lineSplit[0].equalsIgnoreCase("GET")) {
+        if (lineSplit[0].equalsIgnoreCase("GET") && lineSplit[1].contains("?")) {
             //是get请求，需要解析出参数
             String getUrl = lineSplit[1];
             String[] split = getUrl.split("\\?");
@@ -113,8 +155,11 @@ public class SocketProcessor implements Runnable{
                 String[] param = params.split("&");
                 for (String entry : param) {
                     String[] entryArr = entry.split("=");
-                    request.getParams().put(entryArr[0], entryArr[1]);
+                    paramMap.put(entryArr[0], entryArr[1]);
                 }
+
+                //将请求行携带的参数封装进请求对象中
+                request.setParams(paramMap);
             }
         } else {
             request.setUrl(lineSplit[1]);
@@ -124,25 +169,34 @@ public class SocketProcessor implements Runnable{
     }
 
     /**
-    * 解析请求头
-    * */
+     * 解析请求头
+     * */
 
 
     /**
-    * 解析请求体
-    * */
-    private static void getReqBody(String dataStr) {
-        //获取请求体
+     * 解析请求体
+     */
+    private void getReqBody() {
         //提取请求体部分的json字符串，以大括号分割
-        String[] jsonStr = dataStr.split("\\{");
+        JSONObject jsonObject = JSONObject.parseObject(reqBody);
 
-        if (jsonStr.length > 1) {
-            Object parsedObject = JSONObject.parseObject("{" + jsonStr[1]);
+        HashMap<String, Object> bodyMap = new HashMap<>(jsonObject);
 
-            System.out.println(parsedObject);
-        }
+        request.setData(bodyMap);
     }
 
+    /**
+     * 发送响应
+     */
+    public void sendResp() throws Exception {
+
+        //封装响应对象
+        HttpResponse response = new HttpResponse();
+
+        PrintWriter out = response.getWriter();
+
+        out.println(JSONObject.toJSONString(Result.success(request)));
+    }
 
 
 }
---
+++ small-group/Week_2/http-demo/src/main/java/com/example/http/result/Result.java
@@ -0,0 +1,71 @@
+package com.example.http.result;
+
+/**
+ * @author root
+ * @packageName: com.example.http.result
+ * @className: Result
+ * @description: 统一响应结果
+ * @date 2024/3/27 12:28
+ */
+public class Result<T> {
+
+    //状态码
+    private String code;
+
+    //状态描述信息
+    private String msg;
+
+    //响应数据
+    private T data;
+
+    public Result() {
+    }
+
+    public Result(String code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    //请求成功并且返回数据
+    public static <U> Result<U> success(U data){
+
+        return new Result<>("200","success",data);
+    }
+
+    //请求成功，但是没有返回数据
+    public static <U> Result<U> success(){
+
+        return success(null);
+    }
+
+    //请求异常，返回异常信息
+    public static <U> Result<U> error(String code,String msg){
+
+        return new Result<>(code,msg,null);
+    }
+}
